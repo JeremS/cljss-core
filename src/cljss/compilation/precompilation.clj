fe4acbc6d76359a77883222a0d15c5cd3e046ff1
Jérémy Schoffen <jschoffen.dev@gmail.com>
2013-04-07 22:42:37 +0200

    Spliting of the compilation
    Try to separate a precompilation phase from the compilation one.

---
+++ src/cljss/compilation/precompilation.clj
@@ -0,0 +1,102 @@
+(ns cljss.compilation.precompilation
+  (:require [cljss.selectors :as sel]
+            [clojure.string :as string])
+  (:use cljss.data))
+
+(defrecord Decorator [f env])
+
+(defn decorator
+  "Construct a decorator, is a function that 
+  decorate, transforms a rule given a environment.
+  the environment env, must be a map.
+  
+  When no environment is provided
+  an empty map is used as the default one."
+  ([f]
+   (Decorator. f {}))
+  ([f env]
+   (Decorator. f env)))
+
+
+(defn- chain-2-decorators [d1 d2]
+  (let [{f1 :f env1 :env} d1
+        {f2 :f env2 :env} d2]
+    (decorator
+     (fn [r env]
+       (let [[r env] (f1 r env)]
+         (f2 r env)))
+     (merge env1 env2))))
+
+(defn chain-decorators 
+  "Allows to compose from left to right
+  the behaviour of decorators.
+  
+  Be careful, the default environments of each
+  decorators are merged, if they "
+  [d1 d2 & ds]
+  (reduce chain-2-decorators 
+          (list* d1 d2 ds)))
+
+(defn- dr [r f env]
+  (let [[new-r new-env] (f r env)
+        new-sub-rules (map #(dr % f new-env)
+                          (:sub-rules r))]
+    (assoc new-r 
+      :sub-rules new-sub-rules)))
+
+(defn decorate-rule 
+  "Applies a decorator to a rule and recursively 
+  to its sub rules."
+  [r {:keys [f env]}]
+  (dr r f env))
+
+(def depth-decorator
+  "Attach to a rule its depth, level in which
+  it is embeded."
+  (decorator 
+   (fn [r {d :depth :as env}]
+     (list (assoc r :depth d)
+           (update-in env [:depth] inc)))
+   {:depth 0}))
+
+(def ancestors-decorator
+  "Attach to a rule its chain of ancestors."
+  (decorator
+   (fn [r {ps :ancestors :as env}]
+     (list (assoc r :ancestors ps)
+           (update-in env [:ancestors] 
+                      conj (assoc r :sub-rules []))))
+   {:ancestors []}))
+
+
+(def default-decorator
+  (chain-decorators ancestors-decorator depth-decorator))
+
+
+(defn- dr [r f env]
+  (let [[new-r new-env] (f r env)
+        new-sub-rules (map #(dr % f  new-env)
+                          (:sub-rules r))]
+    (assoc new-r 
+      :sub-rules new-sub-rules)))
+
+(defn decorate-rule 
+  "Given a rule and a decorator, decorate the rule and its sub rules"
+  [r {:keys [f env]}]
+  (dr r f env))
+
+
+(defn flatten-rule 
+  "Given a rule returns a flatten list of the rule and its
+  sub rules"
+  [{:as r}]
+  (let [new-r (assoc r :sub-rules '())
+        sub-rs (:sub-rules r)]
+    (cons new-r
+          (mapcat flatten-rule sub-rs))))
+
+(defn flatten-rules 
+  "See flatten-rule"
+  [rs]
+  (mapcat #(flatten-rule % 0) rs))
+
--- test/cljss/compilation_test.clj
+++ test/cljss/compilation_test.clj
@@ -3,69 +3,9 @@
   (:use cljss.data
         cljss.parse
         cljss.compilation
+        cljss.compilation.precompilation
         [clojure.pprint :only (pprint)]))
 
-(def r1 [:div :bgcolor :blue])
-(def r2 [:a :color :white])
-(def r3 [:p :color :green])
-(def r4 [:strong :color :black])
-
-
-(def p-r1 (parse-rule r1))
-(def p-r2 (parse-rule r2))
-(def p-r3 (parse-rule r3))
-(def p-r4 (parse-rule r4))
-
-
-(def r (conj r1 r2 (conj r3 r4)))
-(def p-r (parse-rule r))
-
-
-(m/fact "The parents decorator associate each rule with its 
-        a sequence of its parents from top most to the immediate one."
-        (decorate-rule p-r ancestors-decorator)
-        => (assoc p-r1 :ancestors []
-             :sub-rules [(assoc p-r2 :ancestors [p-r1])
-                         (assoc p-r3 :ancestors [p-r1]
-                           :sub-rules [(assoc p-r4 :ancestors [p-r1 p-r3])])]))
-
-(def check-decorator 
-  "this decorator just tags each rule with :check true"
-  (decorator
-   (fn [r env]
-     (list (assoc r :check true)
-           env))))
-
-(m/fact "The chain-decorator chain decorators behavior such as"
-        (decorate-rule p-r (chain-decorators check-decorator 
-                                             ancestors-decorator 
-                                             depth-decorator))
-        => (-> p-r
-               (decorate-rule check-decorator)
-               (decorate-rule ancestors-decorator)
-               (decorate-rule depth-decorator)))
-
-(m/fact "flatten rule flattens a rule and its sub rules..."
-        (flatten-rule p-r) 
-        => (list p-r1 p-r2 p-r3 p-r4))
-
-
-
-(def fp-r 
-  "Here the rule has bean parsed then flattened."
-  (-> p-r 
-  (decorate-rule default-decorator)
-  (flatten-rule )))
-
-(def sfpr
-  "Here we combine the selectors of each rule so that 
-  each rule has its definitive selector."
-  (map definitive-selector fp-r))
-
-
-(m/fact "make-definitive-selector combines the selector of a rule
-        with those of its parents"
-        (map :selector sfpr) => [:div [:div :a] [:div :p] [:div :p :strong]])
 
 
 (m/facts "About Compiling selectors"
