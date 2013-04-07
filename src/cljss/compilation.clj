57d8dc5c3cc1cf764732104b9bd5fbf27ba50864
Jérémy Schoffen <jschoffen.dev@gmail.com>
2013-04-07 19:34:31 +0200

    Compilation done more or less

--- src/cljss/compilation.clj
+++ src/cljss/compilation.clj
@@ -1,15 +1,7 @@
 (ns cljss.compilation
-  (:require [cljss.selectors :as sel])
+  (:require [cljss.selectors :as sel]
+            [clojure.string :as string])
   (:use cljss.data))
-
-(defprotocol CssSelector
-  (compile-as-selector [this]))
-
-(defprotocol CssPropertyName
-  (compile-as-property-name [this]))
-
-(defprotocol CssPropertyValue
-  (compile-as-property-value [this]))
 
 
 (defrecord Decorator [f env])
@@ -123,9 +115,112 @@
       (assoc :selector (make-definitive-selector r))
       (dissoc :ancestors)))
 
-(defn compile-property-map [m]
-  )
 
-(defn compile-rule [{:keys [selector properties subrules]}]
-  (let [compiled-selector   (compile-as-selector selector)
-        compiled-properties (compile-property-map)]))
+(defn compile-seq-then-join [v compile-fn c]
+  (->> v
+       (map compile-fn)
+       (string/join c)))
+
+
+(defprotocol CssSelector
+  (compile-as-selector [this]))
+
+(defn compile-path-sel [sel]
+  (compile-seq-then-join sel 
+                         compile-as-selector
+                         \space))
+
+(defn compile-set-sel [sel]
+  (compile-seq-then-join sel 
+                         compile-as-selector
+                         ", "))
+
+(extend-protocol CssSelector
+  String
+  (compile-as-selector [this] this)
+  
+  clojure.lang.Keyword
+  (compile-as-selector [this] (name this))
+  
+  clojure.lang.PersistentVector
+  (compile-as-selector [this]
+    (compile-path-sel this))
+  
+  clojure.lang.IPersistentSet
+  (compile-as-selector [this]
+    (compile-set-sel this)))
+
+
+(-> #{} type supers)
+
+(defprotocol CssPropertyName
+  (compile-as-property-name [this]))
+
+
+(extend-protocol CssPropertyName
+  clojure.lang.Keyword
+  (compile-as-property-name [this] (name this))
+  
+  String
+  (compile-as-property-name [this] this))
+
+
+(defprotocol CssPropertyValue
+  (compile-as-property-value [this]))
+
+
+(defn compile-seq-property-value [s]
+  (compile-seq-then-join s
+                         compile-as-property-value 
+                         \space))
+
+
+(extend-protocol CssPropertyValue
+  String
+  (compile-as-property-value [this] this)
+  
+  clojure.lang.Keyword
+  (compile-as-property-value [this] (name this))
+  
+  clojure.lang.PersistentVector
+  (compile-as-property-value [this]
+    (compile-seq-property-value this))
+  
+  clojure.lang.PersistentList
+  (compile-as-property-value [this]
+    (compile-seq-property-value this)))
+
+
+
+(def ^:dynamic *end-property-line* "")
+(def ^:dynamic *start-properties* "")
+(def ^:dynamic *end-properties* "")
+(def ^:dynamic *general-indent* "")
+(def ^:dynamic *indent* "")
+(def ^:dynamic *property-indent* "")
+
+(defn compile-property [[p-name p-val]]
+  (let [s-name (compile-as-property-name p-name)
+        s-val  (compile-as-property-value p-val)]
+    (str s-name ": " s-val \; *end-property-line*)))
+
+
+
+(defn- add-property-indent [props]
+  (interleave (repeat (str *general-indent* *property-indent*))
+               props))
+
+(defn compile-property-map [m]
+  (->> m
+       (map compile-property )
+       (add-property-indent )
+       (string/join )))
+
+(defn compile-rule [{:keys [selector properties depth]}]
+  (binding [*general-indent* (apply str (repeat depth *indent*))]
+    (let [compiled-selector   (compile-as-selector selector)
+          compiled-properties (compile-property-map properties)]
+      (str *general-indent* compiled-selector " {" *start-properties*
+                                compiled-properties 
+           *general-indent* "}"))))
+
--- test/cljss/compilation_test.clj
+++ test/cljss/compilation_test.clj
@@ -66,3 +66,91 @@
 (m/fact "make-definitive-selector combines the selector of a rule
         with those of its parents"
         (map :selector sfpr) => [:div [:div :a] [:div :p] [:div :p :strong]])
+
+
+(m/facts "About Compiling selectors"
+         (m/fact "Compiling a String selector gives the same string"
+                 (compile-as-selector "div") => "div"
+                 (compile-as-selector "a") => "a"
+                 (compile-as-selector ".class") => ".class")
+         
+         (m/fact "Compiling a Keyword selector gives the name of the keyword"
+                 (compile-as-selector :div) => "div"
+                 (compile-as-selector :a) => "a"
+                 (compile-as-selector :.class) => ".class")
+         
+         (m/fact "Compiling a path like selector give the path of the compiled selectors"
+                 (compile-as-selector [:div "p" :.class]) => "div p .class")
+         
+         (m/fact "Compiling a set of selectors give the set of the compiled selectors"
+                 (compile-as-selector #{:div ["p" :.class]}) 
+                 => (m/some-checker "div, p .class"
+                                    "p .class, div")))
+
+
+(m/facts "About compiling css property names"
+         (m/fact "it compiles a string to the same string"
+                 (compile-as-property-name "color" => "color"))
+         
+         (m/fact "it compiles a keyword to its name string"
+                 (compile-as-property-name :color) => "color"))
+
+(m/facts "About compiling css property values"
+         (m/fact "it compiles a string to the same string"
+                 (compile-as-property-value "black" => "black"))
+         
+         (m/fact "it compiles a keyword to its name string"
+                 (compile-as-property-value :black) => "black")
+         
+         (m/fact "it compiles vectors and lists to the space separated compilation of their values"
+                 (compile-as-property-value ["1px" :solid :black]) => "1px solid black"))
+
+(m/fact "We can compile properties"
+        (compile-property [:color :blue]) => "color: blue;"
+        (compile-property [:border ["1px" :solid :black]]) => "border: 1px solid black;")
+
+
+(m/fact "We can compile a property map"
+        (compile-property-map {:color :blue
+                               :border ["1px" :solid :black]}) 
+        => (m/some-checker "color: blue;border: 1px solid black;"
+                           "border: 1px solid black;color: blue;"))
+
+
+
+(def r (-> [:div :color :blue
+                 :border ["1px" :solid :black]
+                 :width "100px"]
+           (parse-rule)
+           (decorate-rule default-decorator)
+           (flatten-rule)
+           first
+           definitive-selector))
+
+
+(m/fact "We can compile a rule"
+        (compile-rule r)
+        => (m/some-checker "div {color: blue;border: 1px solid black;}"
+                            "div {border: 1px solid black;color: blue;}"))
+
+
+(comment
+(binding [*end-property-line* "\n"
+          *start-properties* "\n"
+          *end-properties* ""
+          *general-indent* ""
+          *indent* ""
+          *property-indent* "  "]
+  (println (compile-rule (assoc r :depth 3))))
+
+)
+
+
+
+
+
+
+
+
+
+                                           
