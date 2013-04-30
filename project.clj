c3fb47be16b9564844e219dd1a74cf43337a21fc
Jérémy Schoffen <jschoffen.dev@gmail.com>
2013-04-30 19:45:48 +0200

    _

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject cljss "0.2.0"
   :description "DSL to write css rule in clojure."
-  
-  :url ""
-  
+
+  :url "https://github.com/JeremS/cljss"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/algo.generic "0.1.1"]
                  [potemkin "0.2.2"]]
-  
+
   :profiles {:dev
              {:dependencies [[org.clojure/tools.trace "0.7.5"]
                              [midje "1.5.1"]]}})
