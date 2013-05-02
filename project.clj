320ce7b8d8dd0a6d10a009921109c1154e090279
Jérémy Schoffen <jschoffen.dev@gmail.com>
2013-05-02 22:16:40 +0200

    Change of name.

--- project.clj
+++ project.clj
@@ -1,11 +1,8 @@
-(defproject cljss "0.2.0"
+(defproject jeremys/cljss-core "0.2.0"
   :description "DSL to write css rule in clojure."
-
-  :url "https://github.com/JeremS/cljss"
-
+  :url "https://github.com/JeremS/cljss-core"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/algo.generic "0.1.1"]
                  [potemkin "0.2.2"]]
